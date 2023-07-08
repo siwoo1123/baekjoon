e577492ffbb065c94bb38df865f78d6b0d710356
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2023-07-08 11:42:30 +0900

    N과 M 추가!

---
+++ baekjoonRe/15651 N과 M (3).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n,m;
+int v[10] = {false, false, false, false, false, false, false, false, false, false};
+
+void dfs(int h, int c, vector<int> o)
+{
+    if(c>=m)
+    {
+        for(int i=0;i<o.size();i++) cout<<o[i]<<' ';
+        cout<<'\n';
+        return;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        v[i]=true;
+        o.push_back(i);
+        dfs(i,c+1,o);
+        o.pop_back();
+        v[i]=false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>n>>m;
+    vector<int> o;
+    dfs(1, 0, o);
+    
+    return 0;
+}
